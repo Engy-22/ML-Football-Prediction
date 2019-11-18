20610b62a66d1b7db960ee175e463c6971877162
B3brooks5 <45647104+B3brooks@users.noreply.github.com>
2019-11-18 14:51:04 -0500

    Aggrigating data
    can now take the schedule and the S&P+ rankings and aggrigate data into a 2d list of games that have both teams S&P+ stats and the outcome of the game on them

--- GetSchedule.py
+++ GetSchedule.py
@@ -2,9 +2,9 @@ from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as uReq
 
 
-def get_td_tags():
-
-    url = "https://www.espn.com/college-football/team/schedule/_/id/52/season/2018"  # default URL to parse
+def get_td_tags(url):
+    ret = []
+    #url = "https://www.espn.com/college-football/team/schedule/_/id/52/season/2018"  # default URL to parse
 
     uClient = uReq(url)  # getting html info to parse
     page_soup = uClient.read()
@@ -12,31 +12,42 @@ def get_td_tags():
 
     page_soup = soup(page_soup, "html.parser")  # parsing html page
     AllStats = page_soup.findAll("td")
-    return AllStats
+
+    for i in AllStats:
+        ret.append(i.text)
+
+    return ret
 
 
 def get_outcomes(tags):         # returns the name and the outcome of the game on a schedule
-    tags = tags[9:]
     name = parse_name(tags[0])
     outcome = tags[1][0]        # outcome of the game is the first letter in the score
     return name, outcome
 
 
 def parse_name(name):           # removes the 'vs' in front of the name and the ranking if any
-    name = name[3:]
+    name = name[2:]
     for i in name:
         if i.isalpha():
-            return name
+            return name[:-1]
         else:
             name = name[1:]     # if no ranking just return
 
 
+def get_all_outcomes_on_schedule(url):      # main function to call helpers, returns all the games from a given url
+    games = {}
+    tags = get_td_tags(url)         # get all the tags for the list
+    tags = tags[9:]
+    team = []
+
+    for i in range(12):
+        game, outcome = get_outcomes(tags)
+        team.append(game)
+        games[game] = outcome       # add to found so far
+        tags = tags[7:]             # move to next game
+
+    return games, team
+
+
 if __name__=='__main__':
-    test = []
-    tags = get_td_tags()
-    for i in tags:
-        test.append(i.text)
-    print("tags")
-    print(len(test))
-    print(test)
-    get_outcomes(test)
+    dic, teams = get_all_outcomes_on_schedule("")
--- GetStats.py
+++ GetStats.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as uReq
+import GetSchedule
 
 
 def get_SPStats():                  # return all td tags
@@ -15,23 +16,53 @@ def get_SPStats():                  # return all td tags
 
 
 def cull(lst):              # only gets information for name, overall, offense, defense, special teams
-    teams = 130
-    ret = []
-    print(len(lst))
-    for i in range(0, teams):
-        ret.append(lst[0].text)
-        ret.append(lst[5].text)
-        ret.append(lst[7].text)
-        ret.append(lst[9].text)
-        ret.append(lst[11].text)
-        for r in range(15):
-            lst.pop(0)
+    numberOfTeams = len(lst)
+    team = []
+    teams = {}
+    for i in range(0, numberOfTeams, 15):
+        #team.append(lst[0+i].text)
+        team.append(lst[5+i].text)
+        team.append(lst[7+i].text)
+        team.append(lst[9+i].text)
+        team.append(lst[11+i].text)
+        teams[lst[0+i].text] = team         # stats located at the teams name
 
-    return ret
+        team = []   # reset list for next team
+
+    return teams
 
 
-if __name__=='__main__':
-    SPStats = get_SPStats()
-    print(SPStats)
-    SPStats = cull(SPStats)
-    print(SPStats)
+def get_all_teams_rankings():
+    stats = get_SPStats()
+    stats = cull(stats)
+    return stats
+
+
+def compare_get_schedules(team, SP, teams, outcomes):
+    allGames = []
+    game = []
+    for i in range(len(outcomes)):
+        print(teams[i])
+        if teams[i] in SP:
+            game = game + SP[team] + SP[teams[i]]
+            game.append(outcomes[teams[i]])
+            allGames.append(game)
+            print(allGames)
+        else:
+            continue
+
+    return allGames
+
+
+if __name__ == '__main__':
+    SP = get_all_teams_rankings()
+    #print(SP)
+    outcomes, teams = GetSchedule.get_all_outcomes_on_schedule("https://www.espn.com/college-football/team/schedule/_/id/52/season/2018")
+    #print(outcomes)
+    #print(teams)
+
+    print(compare_get_schedules("Florida State", SP, teams, outcomes))
+
+
+
+
